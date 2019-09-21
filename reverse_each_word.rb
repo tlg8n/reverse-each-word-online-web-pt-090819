12ae4ba01b04aef9334c5f73842131030ff4a2ec
John Schmidley <tlg8n@outlook.com>
2019-09-21 11:56:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,8 @@
 def reverse_each_word(string)
-    string.each do |string|
-      puts string.reverse
-    end
+    original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
 end
