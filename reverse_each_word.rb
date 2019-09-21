8b05cfe79f044c4add95f0a7d6aa1d6745533bba
John Schmidley <tlg8n@outlook.com>
2019-09-21 11:58:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,11 @@ def reverse_each_word(string)
   return_array.join(" ")
 end
 
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
+end
